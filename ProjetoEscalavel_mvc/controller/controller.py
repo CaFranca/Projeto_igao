0f9b18c15ad856de30a84b3b86f4ab95d0e76423
Caique <135059792+CaFranca@users.noreply.github.com>
2024-10-11 09:54:53 -0300

    Delete ProjetoEscalavel_mvc directory

--- ProjetoEscalavel_mvc/controller/controller.py
+++
@@ -1,4 +0,0 @@
-from flask import Flask, Blueprint, render_template, request, url_for, redirect
-from model.model import addPessoa, listaPessoas
-
-blueprint_geral = Blueprint("blueprint_daora", __name__)
