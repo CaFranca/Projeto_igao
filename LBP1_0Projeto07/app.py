b9dfe52d1a59035e75c8e11d54c0535acd78e078
Caique <135059792+CaFranca@users.noreply.github.com>
2024-10-18 10:08:57 -0300

    Update app.py

--- LBP1_0Projeto07/app.py
+++ LBP1_0Projeto07/app.py
@@ -3,11 +3,10 @@ from controller.controller import blueprint_default as pagina
 from model.model import MeuMiddleware
 
 app = Flask(__name__)
-app.secret_key = 'top_seguranca'  # Defina uma chave secreta para a sessão
+app.secret_key = 'top_seguranca'
 app.register_blueprint(pagina)
-
+app.wsgi_app = MeuMiddleware(app.wsgi_app) 
 # Registrar o middleware
-app.wsgi_app = MeuMiddleware(app.wsgi_app)
 
 if __name__ == "__main__":
     app.run(debug=True)
